a098520de7df9ff40f661d8a39325e7f1f15b8bf
bibaoke <john@bibaoke.com>
2017-09-07 11:58:19 +0800

    添加 css 选择器参数解析缓存

--- Less.Html/Query/Parser/ParamParser.cs
+++ Less.Html/Query/Parser/ParamParser.cs
@@ -1,28 +1,62 @@
 ﻿//bibaoke.com
 
+using System;
+using System.Collections.Generic;
+
 namespace Less.Html.SelectorParamParser
 {
     internal static class ParamParser
     {
+        private static Dictionary<string, ElementFilter[]> Cache
+        {
+            get;
+            set;
+        }
+
+        static ParamParser()
+        {
+            ParamParser.Cache = new Dictionary<string, ElementFilter[]>();
+        }
+
         internal static ElementFilter[] Parse(string param)
         {
-            ReaderBase reader = new SelectorReader();
+            ElementFilter[] filters;
 
-            Context context = new Context(param);
-
-            reader.Context = context;
-
-            while (true)
+            if (ParamParser.Cache.TryGetValue(param, out filters))
             {
-                reader = reader.Read();
-
-                if (reader.IsNull())
-                {
-                    break;
-                }
+                return filters;
             }
+            else
+            {
+                ReaderBase reader = new SelectorReader();
 
-            return context.FilterList.ToArray();
+                Context context = new Context(param);
+
+                reader.Context = context;
+
+                while (true)
+                {
+                    reader = reader.Read();
+
+                    if (reader.IsNull())
+                    {
+                        break;
+                    }
+                }
+
+                filters = context.FilterList.ToArray();
+
+                try
+                {
+                    ParamParser.Cache.Add(param, filters);
+                }
+                catch (ArgumentException)
+                {
+                    //
+                }
+
+                return filters;
+            }
         }
     }
 }
