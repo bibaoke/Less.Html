c0f69ee2ed1952a9f084f82d7d0b3f9317083435
bibaoke <john@bibaoke.com>
2019-12-17 11:58:04 +0800

    修复元素索引

--- Less.Html/Nodes/Element/Indexes/IndexBase.cs
+++ Less.Html/Nodes/Element/Indexes/IndexBase.cs
@@ -8,30 +8,37 @@ namespace Less.Html
     {
         protected void Insert(List<Element> list, Element element)
         {
-            int end = list.Count - 1;
-
-            for (int i = end; i >= 0; i--)
+            if (list.Count > 0)
             {
-                if (element.Index > list[i].Index)
+                int end = list.Count - 1;
+
+                for (int i = end; i >= 0; i--)
                 {
-                    if (i == end)
+                    if (element.Index > list[i].Index)
                     {
-                        list.Add(element);
+                        if (i == end)
+                        {
+                            list.Add(element);
+                        }
+                        else
+                        {
+                            list.Insert(i + 1, element);
+                        }
+
+                        break;
                     }
                     else
                     {
-                        list.Insert(i + 1, element);
-                    }
-
-                    break;
-                }
-                else
-                {
-                    if (i == 0)
-                    {
-                        list.Insert(0, element);
+                        if (i == 0)
+                        {
+                            list.Insert(0, element);
+                        }
                     }
                 }
+            }
+            else
+            {
+                list.Add(element);
             }
         }
     }
